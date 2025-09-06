aaaa078baeb5cbb0fbb5340ca5cc861067322f09
Hend Ahmed <92755637+hend-abdelazim@users.noreply.github.com>
2025-09-07 02:14:25 +0300

    [242] [Time Beats: 20.31%] [Memory Beats: 74.03%] - LeetPush

---
+++ Hashmap/Valid-Anagram.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+        map<char, int> m;
+        for (char c : s) m[c]++;
+        for (char c : t) {
+            if(m.find(c) == m.end() || m[c] == 0) return false;
+            m[c]--;
+        }
+        return true;
+    }
+};
