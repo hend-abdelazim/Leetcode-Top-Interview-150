5d65c6efb47c61d43ae399b11f1301148e65860c
Hend Ahmed <92755637+hend-abdelazim@users.noreply.github.com>
2025-10-07 03:04:05 +0300

    [20] [Time Beats: 8.56%] [Memory Beats: 32.57%] - LeetPush

---
+++ Stack/Valid-Parentheses.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(auto c : s) {
+            if(c == '(' || c == '[' || c == '{')
+                st.push(c);
+            else if((c == ')' || c == ']' || c == '}') && st.empty())
+                return false;
+            else if((c == ')' && st.top() == '(') || (c == '}' && st.top() == '{') || (c == ']' && st.top() == '['))
+                st.pop();
+            else
+                return false;
+        }
+        return st.empty();
+    }
+};
